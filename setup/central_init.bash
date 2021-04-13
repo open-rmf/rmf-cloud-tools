029028e234da1aa03e902ae3826de7cb2c387a4f
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-04-13 15:59:44 +0800

    fix non-functional docker apt repo
    Signed-off-by: Boon Han <charayaphan.nakorn.boon.han@gmail.com>

--- setup/central_init.bash
+++ setup/central_init.bash
@@ -24,10 +24,9 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
   curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-  add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+  echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   apt update
   apt install docker-ce docker-ce-cli containerd.io -y
