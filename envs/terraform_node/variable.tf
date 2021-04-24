d06050fa56772387e1a3f5b9cf5221f2a1f0af77
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-04-24 15:27:12 +0800

    add flag to change server name

--- envs/terraform_node/variable.tf
+++ envs/terraform_node/variable.tf
@@ -1,6 +1,6 @@
 variable "standard_tags" {
   default = {
-    project = "terraform_node"
+    project = "terraform_node"      # Change here with your preferred name
     environment = "development"
   }
   description = "Tags to be applied to all resources"
