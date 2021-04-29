a950bd9bf2d4ec49d6b41ff6b265fecc0f48b779
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-04-29 17:13:29 +0000

    add sample slack integration
    Signed-off-by: Boon Han <charayaphan.nakorn.boon.han@gmail.com>

--- scripts/README.md
+++ scripts/README.md
@@ -4,4 +4,9 @@ Helpful Scripts to test RMF with.
 ## task_generator.py
 ```
  python3 task_generator.py --robot_type tinyRobot --task_config_path ../random_loops/loop_points.yml  --task_type loop --use_sim_time --task_check_timeout 300.0 
+
+ # If you want to try Slack integration, you need a API token and uncomment the relevant paragraph in the _handle_task_failure function
+ # https://github.com/slackapi/python-slack-sdk/blob/main/tutorial/01-creating-the-slack-app.md
+SLACK_BOT_TOKEN=[your-token] SLACK_BOT_CHANNEL=[#your-channel-with-hash] python3 task_generator.py [args]
 ```
+
--- scripts/random_tasks/task_generator.py
+++ scripts/random_tasks/task_generator.py
@@ -204,6 +204,25 @@ class TaskGenerator:
     log_process.kill()
     # Implement your own alert methods here 
     subprocess.Popen(['mv', f'{self.config.log_label}/{task_to_track}.bag', f'{self.config.log_label}/{task_to_track}-failed.bag'])
+
+    # Example slack integration
+    # import os
+    # from slack_sdk import WebClient
+    # from slack_sdk.errors import SlackApiError
+
+    # client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+
+    # try:
+        # subprocess.Popen(['zip', '-r', f'{self.config.log_label}/{task_to_track}-failed.zip', f'{self.config.log_label}/{task_to_track}-failed.bag']).communicate()
+        # filepath=f'{self.config.log_label}/{task_to_track}-failed.zip'
+        # response = client.files_upload(channels=os.environ['SLACK_BOT_CHANNEL'], file=filepath)
+        # assert response["file"]  # the uploaded file
+    # except SlackApiError as e:
+        # # You will get a SlackApiError if "ok" is False
+        # assert e.response["ok"] is False
+        # assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        # print(f"Got an error: {e.response['error']}")
+
     shutdown(1)
 
   def _handle_task_success(self, task_to_track, log_process):
